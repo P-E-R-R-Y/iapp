94076437d045baab32900d47e807a7d56ec640f3
Chouteau Perry <perry.chouteau@epitech.eu>
2025-06-29 15:36:30 +0200

    feat(iapp): app interface.

---
+++ includes/IApp.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file IApp.hpp
+ * @author Perry Chouteau (perry.chouteau@outlook.com)
+ * @brief
+ * @date 2025-06-29
+ */
+
+#ifndef IAPP_HPP_
+    #define IAPP_HPP_
+
+    class IApp {
+        public:
+            virtual int start() = 0;
+    };
+
+#endif /* IAPP_HPP_ */
